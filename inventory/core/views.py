5b3dab1a6c95744641995472424fb78f82e30827
mram <mram@umd.edu>
2019-04-25 13:15:19 -0400

    Latest changes

--- inventory/core/views.py
+++ inventory/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import json
+
 
 # Create your views here.
+def inventory(request):
+    print(request.data)
+
+    data = json.dumps({
+        'type': 'banana',
+    })
+
+    return HttpResponse(data, content_type='application/json')
