16e3bd50ab02e3212cc0c5be479886530cdb032f
mram <mram@umd.edu>
2019-04-25 15:11:59 -0400

    Spreadsheet

--- inventory/core/views.py
+++ inventory/core/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import pandas as pd
 import json
 
 
+SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTFcaWz8ylGNayO5HkZBRTuU6fTmGLXLW1NVscDFL0AQvIWU3hbzatm_oZt_J5AyLkT86qizFbyvsNC/pub?output=xlsx'
+
 # Create your views here.
 def inventory(request):
-    room = request.POST['text'].split()[0]
+    room = int(request.POST['text'].split()[0])
+
+    file = pd.read_excel(SPREADSHEET_URL)
+
+    text = f'*Room:* {room}\n*Status*: All out of everything.\n'
+    text += file[['Item', room]]
 
     data = json.dumps({
-        'text': f'*Room:* {room}\n*Status*: All out of everything.\n',
+        'text': text,
     })
 
     return HttpResponse(data, content_type='application/json')
